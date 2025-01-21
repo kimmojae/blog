bae3ca87c3bdafa2e05c58760976c142c1848d0c
kimmojae <mojaekim@gmail.com>
2025-01-21 16:32:55 +0900

    color mode selector 컴포넌트 구현

---
+++ app/components/color-mode-selector.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    class="inline-flex items-center justify-center p-2 rounded-sm hover:bg-gray-200 dark:hover:bg-[#334555]"
+    @click="toggleMode"
+  >
+    <Icon :name="nextModeIcon" />
+  </button>
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+
+const nextModeIcons = {
+  light: 'heroicons:sun',
+  dark: 'heroicons:moon',
+}
+
+const nextMode = computed(() => {
+  if (colorMode.preference === 'system') {
+    return colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+  return colorMode.preference === 'light' ? 'dark' : 'light'
+})
+
+const nextModeIcon = computed(() => nextModeIcons[nextMode.value])
+
+const toggleMode = () => (colorMode.preference = nextMode.value)
+</script>
