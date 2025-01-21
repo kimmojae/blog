97142993ea8b20c20d2b13acb660cff47e8180f0
kimmojae <mojaekim@gmail.com>
2025-01-21 16:33:20 +0900

    default layout 구현

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,31 +1,39 @@
 <template>
-  <div class="container mx-auto max-w-4xl">
-    <header class="flex justify-between items-start md:items-center mt-5">
-      <div class="flex items-center md:space-x-12">
-        <div class="hidden md:block">
-          <NuxtLink to="/" class="text-xl font-semibold p-2"
-            >Kimmojae
-          </NuxtLink>
-        </div>
+  <div class="m-auto max-w-2xl p-6 pt-3 md:pt-6 min-h-screen">
+    <header class="flex mb-5 md:mb-10 items-center">
+      <NuxtLink to="/" class="text-md md:text-lg whitespace-nowrap font-bold"
+        >Kimmojae
+      </NuxtLink>
 
-        <!-- <Menu /> -->
-      </div>
+      <nav class="flex grow justify-end items-center gap-1 md:gap-3">
+        <NuxtLink
+          to="/about"
+          class="inline-flex hover:bg-gray-200 dark:hover:bg-[#334555] rounded-sm p-2 text-xs"
+          >About
+        </NuxtLink>
 
-      <!-- <ClientOnly>
-        <ColorModeSelector />
-      </ClientOnly> -->
+        <a
+          href="https://github.com/kimmojae"
+          target="_blank"
+          class="inline-flex hover:bg-gray-200 dark:hover:bg-[#334555] rounded-sm p-2"
+        >
+          <Icon name="uil:github" />
+        </a>
+
+        <ClientOnly>
+          <ColorModeSelector />
+        </ClientOnly>
+      </nav>
     </header>
 
-    <main class="p-2 mt-10">
+    <main class="max-w-2xl m-auto mb-10 text-sm">
       <slot />
     </main>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 useHead({
   titleTemplate: '%s - Kimmojae',
 })
 </script>
-
-<style></style>
